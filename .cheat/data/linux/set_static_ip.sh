ff26bfb4be221aceff44138a30c8984d074ffebc
Kevin De Koninck <kevin.de.koninck92@gmail.com>
2020-12-01 10:12:06 +0100

    Create set_static_ip.sh

---
+++ .cheat/data/linux/set_static_ip.sh
@@ -0,0 +1,10 @@
+vi /etc/sysconfig/network-scripts/ifcfg-ens192 
+
+  # change and/or add the following
+      BOOTPROTO=none   # change
+      IPADDR=10.50.235.237
+      NETMASK=255.255.252.0
+      GATEWAY=10.50.232.1
+      DNS1=173.38.200.100
+      ONBOOT=yes  # change
+      
