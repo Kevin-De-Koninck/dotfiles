107c7b719e1f604497693ac949c1a7e3023ec6db
Kevin De Koninck <kevin.de.koninck92@gmail.com>
2020-12-01 10:08:35 +0100

    Update df.sh

--- .cheat/data/commands/df.sh
+++ .cheat/data/commands/df.sh
@@ -1,3 +1,1 @@
-du -h /path                       # disk usage by files
-du -h --max-depth=3 /path         # disk usage by files but only x levels deep
-du -h -s /path                    # disk usage by files but summurize (= --max-depth=0)
+df -h /path                       # disk space free
