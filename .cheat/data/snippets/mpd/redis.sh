e194a9961d3b155f742224eb75cf467919752437
Kevin De Koninck <kdekoninck@synamedia.com>
2021-01-05 17:20:59 +0100

    Remove duplicate data

--- .cheat/data/snippets/mpd/redis.sh
+++
@@ -1,9 +0,0 @@
-# Remove redis locks
-# Go to: go to https://os2master01.os2.ino.krk.synamedialabs.com:8443/console/project/henk-redis/browse/pods/redis-master-1874722804-k24wq?tab=terminal (system/admin)
-# Open the terminal and type:
-bash
-redis-cli
-select 10
-KEYS *                # Shows all locks
-FLUSHDB
-KEYS *
