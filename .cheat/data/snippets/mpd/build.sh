82a2067a91bef3e58aa39a079c05995df87085a0
Kevin De Koninck <kdekoninck@synamedia.com>
2021-07-07 15:51:22 +0200

    update

--- .cheat/data/snippets/mpd/build.sh
+++ .cheat/data/snippets/mpd/build.sh
@@ -11,7 +11,8 @@
 VERSION_OLD=$(cat buildimages/deployer-base/buildvars | sed -n 's|^DST_VERSION="\(.*\)"$|\1|p')
 VERSION_NEW=$(echo $VERSION_OLD | cut -d '-' -f 1)-$(($(echo $VERSION_OLD | cut -d '-' -f 2) + 1))
 ls -1 buildimages/deployer*/{Dockerfile,buildvars} | xargs sed -i "s/${VERSION_OLD}/${VERSION_NEW}/g"
-git add buildimages/deployer-base/buildvars buildimages/deployer-dev-base/Dockerfile buildimages/deployer-dev-base/buildvars buildimages/deployer-dev/Dockerfile buildimages/deployer-dev/buildvars buildimages/deployer-jenkins/Dockerfile buildimages/deployer-jenkins/buildvars buildimages/deployer/Dockerfile
+git add buildimages/*
 git commit -m "Update version to ${VERSION_NEW}"
-./tools/build/build.sh -p deployer-base -r
-./tools/build/build.sh -p deployer-dev-base -r
+./tools/build/build.sh -p deployer-base --no-cache --push
+./tools/build/build.sh -p deployer-dev-base --no-cache --push
+
